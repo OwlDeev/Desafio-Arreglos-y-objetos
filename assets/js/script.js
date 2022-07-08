a0954c4beaf16e68069ebd3cb81d883f951ce020
OwlDeev <82920284+OwlDeev@users.noreply.github.com>
2022-07-08 13:05:12 -0400

    c3 - arreglos debugger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 //DEFINICION DE VARIABLES
 
-var sidebar = document.getElementsByClassName("d-flex align-items-center justify-content-around flex-column sidebar")[0];
+var sidebar = document.getElementsByClassName(
+  "d-flex align-items-center justify-content-around flex-column sidebar"
+)[0];
 const inputCantidadCuartos = document.querySelector("#inputCantidadCuartos");
 const inputDesdeMc = document.querySelector("#inputDesdeMc");
 const inputHasta = document.querySelector("#inputHastaMc");
@@ -14,7 +16,7 @@ inputHasta.value = 0;
 let contadorAlertaCuartos = 0;
 let contadorAlertaMetros = 0;
 let contadorAlertaNull = false;
-var titleError = '';
+var titleError = "";
 
 //FUNCIONES Y METODOS
 sidebar.style.backgroundImage = "url('assets/imgs/fondoFiltro.png')";
@@ -67,18 +69,24 @@ const propiedades = [
     src: "...",
     cuartos: 2,
     metros: 700,
-  }
+  },
 ];
 
 const propiedadesSection = document.querySelector("#propiedadesSection");
 
 function updatePropiedades() {
-  let propiedadesUpdate = '';
+  let propiedadesUpdate = "";
   let contador = 0;
   for (let propiedad of propiedades) {
     let metrosSolicitados = Number(inputDesdeMc.value * inputHasta.value);
-    if(propiedad.cuartos <= inputCantidadCuartos.value && inputCantidadCuartos.value != 0){
-      if((propiedad.metros >= inputDesdeMc.value) && (propiedad.metros <=inputHasta.value)){
+    if (
+      propiedad.cuartos <= inputCantidadCuartos.value &&
+      inputCantidadCuartos.value != 0
+    ) {
+      if (
+        propiedad.metros >= inputDesdeMc.value &&
+        propiedad.metros <= inputHasta.value
+      ) {
         propiedadesUpdate += `
         <div class="p-2">
         <div class="card" id="itemGalleryTablerDesktop">
@@ -98,7 +106,7 @@ function updatePropiedades() {
                 </div>
                 <div class="flexColumn">
                   <p>Metros: </p>
-                  <p id="paddingLeft">${(propiedad.metros)}</p>
+                  <p id="paddingLeft">${propiedad.metros}</p>
                 </div>
               </div>
               <p class="card-text">${propiedad.descripcion}</p>
@@ -111,59 +119,95 @@ function updatePropiedades() {
         contador++;
         contadorAlertaMetros++;
       }
-      
       contadorAlertaCuartos++;
-      }else{
-        if(propiedad.metros <= metrosSolicitados){
-          contadorAlertaMetros++;
-        }
+    } else {
+      if (propiedad.metros <= metrosSolicitados) {
+        contadorAlertaMetros++;
       }
-    
+    }
   }
-  if(contador == 0){
+  if (contador == 0) {
     contadorAlertaNull = true;
   }
   alertError();
-  pNumeroTotal.innerText = 'Total encontrado: '+ contador;
+  pNumeroTotal.innerText = "Total encontrado: " + contador;
   return propiedadesUpdate;
-};
+}
 
 const buttonSidebar = document.querySelector("#buttonSidebar");
 buttonSidebar.addEventListener("click", function update() {
-  propiedadesSection.innerHTML = updatePropiedades();
+  if (alertError() == true) {
+    propiedadesSection.innerHTML = updatePropiedades();
+  } else {
+    console.log("no pasa por el codigo");
+  }
   limpiarFiltro();
 });
 
-
-function alertError(){
-console.log('cuartos: '+contadorAlertaCuartos);
-console.log('metros: '+contadorAlertaMetros);
-console.log('vacio: '+contadorAlertaNull);
-  if(contadorAlertaNull == true && contadorAlertaCuartos > 0 && contadorAlertaMetros == 0){
-    titleError = 'Error';
-    titleDescription = 'No se encontro ninguna propiedad con esa cantidad de metros'
+function alertError() {
+  
+  if (
+    contadorAlertaNull == true &&
+    contadorAlertaCuartos > 0 &&
+    contadorAlertaMetros == 0
+  ) {
+    titleError = "Error";
+    titleDescription =
+      "No se encontro ninguna propiedad con esa cantidad de metros";
     showModal();
-  }else if(contadorAlertaNull == true && contadorAlertaCuartos == 0 && contadorAlertaMetros > 0){
-    titleError = 'Error';
-    titleDescription = 'Existen propiedades con los metros seleccionados pero con mas cuartos'
+    return false;
+  } else if (
+    contadorAlertaNull == true &&
+    contadorAlertaCuartos == 0 &&
+    contadorAlertaMetros > 0
+  ) {
+    titleError = "Error";
+    titleDescription =
+      "Existen propiedades con los metros seleccionados pero con mas cuartos";
     showModal();
-  }else if((contadorAlertaNull == true && contadorAlertaCuartos == 0 && contadorAlertaMetros == 0) && (inputCantidadCuartos.value != '0' || inputDesdeMc.value != '0' || inputHasta.value != '0')){
-    titleError = 'Error';
-    titleDescription = 'Debe seleccionar al menos 1 cuarto'
+    return false;
+  } else if (
+    contadorAlertaNull == true &&
+    contadorAlertaCuartos == 0 &&
+    contadorAlertaMetros == 0 &&
+    (inputCantidadCuartos.value != "0" ||
+      inputDesdeMc.value != "0" ||
+      inputHasta.value != "0")
+  ) {
+    titleError = "Error";
+    titleDescription = "Debe seleccionar al menos 1 cuarto";
     showModal();
-  }else if(contadorAlertaNull == true && contadorAlertaCuartos == 0 && contadorAlertaMetros == 0){
-    titleError = 'Error';
-    titleDescription = 'Debe seleccionar al menos una caracteristica del filtro'
+    return false;
+  } else if (
+    contadorAlertaNull == true &&
+    contadorAlertaCuartos == 0 &&
+    contadorAlertaMetros == 0
+  ) {
+    titleError = "Error";
+    titleDescription =
+      "Debe seleccionar al menos una caracteristica del filtro";
     showModal();
+    return false;
+  } else if (
+    inputDesdeMc.value > inputHasta.value ||
+    inputHasta.value < inputDesdeMc.value
+  ) {
+    titleError = "Error Filtro";
+    titleDescription = 'No puede ser el filtro "Desde" mayor que "Hasta"';
+    showModal();
+    return false;
   }
+  console.log("cuartos: " + contadorAlertaCuartos);
+  console.log("metros: " + contadorAlertaMetros);
+  console.log("vacio: " + contadorAlertaNull);
 }
 
 //MOSTRAR TODAS LAS PROPIEDADES
 linkAllPropiedades.addEventListener("click", function allPropiedades() {
-  let propiedadesUpdate = '';
+  let propiedadesUpdate = "";
   let contador = 0;
   for (let propiedad of propiedades) {
-        propiedadesUpdate += `
+    propiedadesUpdate += `
         <div class="p-2">
         <div class="card" id="itemGalleryTablerDesktop">
           <img
@@ -182,7 +226,7 @@ linkAllPropiedades.addEventListener("click", function allPropiedades() {
                 </div>
                 <div class="flexColumn">
                   <p>Metros: </p>
-                  <p id="paddingLeft">${(propiedad.metros)}</p>
+                  <p id="paddingLeft">${propiedad.metros}</p>
                 </div>
               </div>
               <p class="card-text">${propiedad.descripcion}</p>
@@ -192,19 +236,19 @@ linkAllPropiedades.addEventListener("click", function allPropiedades() {
         </div>
         </div>
         `;
-        contador++;
+    contador++;
   }
-  pNumeroTotal.innerText = 'Total: Hay '+ propiedades.length+' esperando tu llegada';
+  pNumeroTotal.innerText =
+    "Total: Hay " + propiedades.length + " esperando tu llegada";
   propiedadesSection.innerHTML = propiedadesUpdate;
-  console.log('pasa')
-})
+  console.log("pasa");
+});
 
 //MODAL
 var modalWrap = null;
-const showModal = () =>{
-
-  modalWrap = document.createElement('div');
-  modalWrap.innerHTML =`
+const showModal = () => {
+  modalWrap = document.createElement("div");
+  modalWrap.innerHTML = `
   <div class="modal" tabindex="-1">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -223,12 +267,12 @@ const showModal = () =>{
   `;
 
   document.body.append(modalWrap);
-  var modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+  var modal = new bootstrap.Modal(modalWrap.querySelector(".modal"));
   modal.show();
-}
+};
 
 //LIMPIAR VARIABLES
-function limpiarFiltro(){
+function limpiarFiltro() {
   contadorAlertaCuartos = 0;
   contadorAlertaMetros = 0;
   contadorAlertaNull = false;
@@ -236,3 +280,62 @@ function limpiarFiltro(){
   inputDesdeMc.value = 0;
   inputHasta.value = 0;
 }
+
+//VALIDACIONES DE CAMPO NUMERICO
+class CampoNumerico {
+  constructor(selector) {
+    this.nodo = document.querySelector(selector);
+    this.valor = "";
+
+    this.empezarAEscucharEventos();
+  }
+
+  empezarAEscucharEventos() {
+    this.nodo.addEventListener(
+      "keydown",
+      function (evento) {
+        const teclaPresionada = evento.key;
+        const teclaPresionadaEsUnNumero = Number.isInteger(
+          parseInt(teclaPresionada)
+        );
+
+        const sePresionoUnaTeclaNoAdmitida =
+          teclaPresionada != "ArrowDown" &&
+          teclaPresionada != "ArrowUp" &&
+          teclaPresionada != "ArrowLeft" &&
+          teclaPresionada != "ArrowRight" &&
+          teclaPresionada != "Backspace" &&
+          teclaPresionada != "Delete" &&
+          teclaPresionada != "Enter" &&
+          !teclaPresionadaEsUnNumero;
+        const comienzaPorCero =
+          this.nodo.value.length === 0 && teclaPresionada == 0;
+
+        if (sePresionoUnaTeclaNoAdmitida || comienzaPorCero) {
+          evento.preventDefault();
+        } else if (teclaPresionadaEsUnNumero) {
+          this.valor += String(teclaPresionada);
+        }
+      }.bind(this)
+    );
+
+    this.nodo.addEventListener(
+      "input",
+      function (evento) {
+        const cumpleFormatoEsperado = new RegExp(/^[0-9]+/).test(
+          this.nodo.value
+        );
+
+        if (!cumpleFormatoEsperado) {
+          this.nodo.value = this.valor;
+        } else {
+          this.valor = this.nodo.value;
+        }
+      }.bind(this)
+    );
+  }
+}
+
+new CampoNumerico("#inputDesdeMc");
+new CampoNumerico("#inputHastaMc");
+new CampoNumerico("#inputCantidadCuartos");
